3faa8c6af10cebacb5236e933e58ee603958c0ed
Ernesto-0 <ernestoguerrero2023@outlook.es>
2024-12-24 15:50:05 -0500

    Mapa

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -28,7 +28,6 @@
               width="100%" 
               height="100%" 
               style="border:0;" 
-              allowfullscreen="" 
               loading="lazy"
             ></iframe>
           </div>
